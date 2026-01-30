403a7d0a20fedbb35658df70bce4d2da30ee5706
ThisisaBurnerAccount <9961699@bedfordnhk12.net>
2026-01-30 23:43:04 +0000

    finished problem 1

--- Problem1.java
+++ Problem1.java
@@ -1,5 +1,17 @@
 public class Problem1 {
     public static void main(String[] args) {
-        System.out.println("IGOTHERE");
+        int add = 0;
+        for (int i = 1; i < 1000; i++) {
+            System.out.println(i);
+            if (i % 3 == 0) {
+                System.out.println("divisible by 3");
+                add = add + i;
+            }
+            if (i % 5== 0) {
+                System.out.println("divisible by 5");
+                add = add + i;
+            }
+            System.out.println("sum: "+add);
+        }
     }
 }
